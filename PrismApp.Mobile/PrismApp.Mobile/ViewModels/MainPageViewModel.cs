f913f5b0a2711e6c5a98b1547d4a0252f7e711c2
Patrick <patrick.rainer2@gmail.com>
2021-08-28 21:56:45 +0200

    initial commit

---
+++ PrismApp.Mobile/PrismApp.Mobile/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using PrismApp.Mobile.Commands;
+using PrismApp.Mobile.Views;
+
+namespace PrismApp.Mobile.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        string _title;
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public DelegateCommand<object> DelegateButtonCommand
+        {
+            get;
+            private set;
+        }
+
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Main Page";
+            DelegateButtonCommand = new DelegateCommand<object>(Submit, CanSubmit);
+        }
+
+        bool CanSubmit(object arg)
+        {
+            return true;
+        }
+
+        void Submit(object obj)
+        {
+            Title = "Button pressed";
+        }
+    }
+}
