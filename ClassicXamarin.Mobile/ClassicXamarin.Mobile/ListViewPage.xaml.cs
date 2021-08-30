2b764c8d5eb7fd73d2aba2838dbc184dbc61002f
Patrick <patrick.rainer2@gmail.com>
2021-08-30 08:23:06 +0200

    Added some labels

--- ClassicXamarin.Mobile/ClassicXamarin.Mobile/ListViewPage.xaml.cs
+++ ClassicXamarin.Mobile/ClassicXamarin.Mobile/ListViewPage.xaml.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using System.Collections.ObjectModel;
 using Xamarin.Forms.Xaml;
 
 namespace ClassicXamarin.Mobile
